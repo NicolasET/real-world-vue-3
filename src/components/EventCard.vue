cba95998618d4b47cd1795309a6997c0b244674f
NicolasET <nicolaset98@gmail.com>
2023-11-15 22:23:24 -0500

    C3

---
+++ src/components/EventCard.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+interface Props {
+  event: Event;
+}
+
+interface Event {
+  id: number;
+  category: string;
+  title: string;
+  description: string;
+  location: string;
+  date: string;
+  time: string;
+  petsAllowed: boolean;
+  organizer: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+  <div class="event-card">
+    <h2>{{ event.title }}</h2>
+    <span>@{{ event.time }} on {{ event.date }}</span>
+  </div>
+</template>
+
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid #39495c;
+  margin-bottom: 18px;
+}
+
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+}
+</style>
